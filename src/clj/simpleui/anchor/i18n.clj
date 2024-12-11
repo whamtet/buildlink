1df61dc09fa2a92b9b4dc20926275715936f5da4
Matthew Molloy <whamtet@gmail.com>
2024-12-11 20:58:43 +0900

    htmx

---
+++ src/clj/simpleui/anchor/i18n.clj
@@ -0,0 +1,61 @@
+(ns simpleui.anchor.i18n
+    (:require
+      [clojure.data.json :as json]
+      [clojure.java.io :as io]
+      [clojure.string :as string]
+      [simpleui.anchor.web.middleware.i18n :as middleware.i18n]
+      [simpleui.anchor.util :as util :refer [defm-dev]]))
+
+(defn- zip-pairs [s]
+  (zipmap
+   (take-nth 2 s)
+   (take-nth 2 (rest s))))
+
+(defn slurp-translation [lang]
+  (->> lang
+       (format "i18n/%s.txt")
+       io/resource
+       slurp))
+
+(defm-dev extract-translation [lang]
+  (if lang
+    (-> lang
+        slurp-translation
+        (.split "\n")
+        (->>
+         (map #(.trim %))
+         (filter not-empty)
+         zip-pairs))
+    {}))
+
+(def phrases #{})
+
+(defn- trimmed? [^String s]
+  (= s (.trim s)))
+
+(defmacro i18n [^String s]
+  (assert (trimmed? s))
+  (alter-var-root #'phrases conj s)
+  `((extract-translation middleware.i18n/*lang*) ~s ~s))
+
+(defmacro i18ns [& ss]
+  (assert (every? trimmed? ss))
+  (alter-var-root #'phrases #(apply conj % ss))
+  `(let [m# (extract-translation middleware.i18n/*lang*)]
+    (mapv #(m# % %) (list ~@ss))))
+
+(defmacro i18n-map [m]
+  (assert (map? m))
+  (let [new-phrases (vals m)]
+    (assert (every? trimmed? new-phrases))
+    (alter-var-root #'phrases #(apply conj % new-phrases))
+    `(let [m# (extract-translation middleware.i18n/*lang*)]
+      (json/write-str
+       (util/map-vals #(m# % %) ~m)))))
+
+(defn spit-phrases []
+  (->> phrases
+       sort
+       (map #(str % "\n"))
+       (string/join "\n")
+       (spit "i18n.txt")))
---
+++ src/clj/simpleui/anchor/web/middleware/i18n.clj
@@ -0,0 +1,8 @@
+(ns simpleui.anchor.web.middleware.i18n)
+
+(def ^:dynamic *lang* nil)
+
+(defn set-lang [handler]
+  (fn [req]
+    (binding [*lang* (-> req :session :lang)]
+      (handler req))))
